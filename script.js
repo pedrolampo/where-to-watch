867e35ca850fba67d25440c4954a76f36f6ce34b
Pedro Sanchez Lampolia <p.sanchez.lampolia@viseven.com>
2022-05-04 16:44:49 -0300

    finished?

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const input = document.querySelector('[data-input]');
 const searchBtn = document.querySelector('[data-search]');
-const resultsContainer = document.querySelector('[data-results]');
+const clearBtn = document.querySelector('[data-clear]');
+const resultsContainer = document.getElementById('results');
+
+const movieCheckbox = document.querySelector('[data-movie-checkbox]');
+const tvCheckbox = document.querySelector('[data-tv-checkbox]');
 
 const API_KEY = '469vh9ZIpXnNbLdTR0yzFVwKCzFIHExEutPqM5yT';
 
 const options = {
   method: 'GET',
-  headers: {
-    'X-RapidAPI-Host': 'streaming-availability.p.rapidapi.com',
-    'X-RapidAPI-Key': 'fd31e0f5bbmsh9cd8cbe53d8426ap1fcc64jsne202d1690d3d',
-  },
 };
 
 let search = '';
@@ -20,6 +20,7 @@ searchBtn.addEventListener('click', (e) => {
   // Clean previous results
   let resultsDivs = resultsContainer.children;
   for (let item of resultsDivs) {
+    clearBtn.click();
     item.remove();
   }
 
@@ -31,47 +32,68 @@ searchBtn.addEventListener('click', (e) => {
     let url = `https://api.watchmode.com/v1/search/?apiKey=${API_KEY}&search_field=name&search_value=${search}`;
     let results = [];
 
-    fetch(url, { method: 'Get' })
-      .then((res) => res.json())
-      .then((json) => {
-        results = json.title_results;
-        results.forEach((title) => {
-          // Get where to watch
-          fetch(
-            `https://streaming-availability.p.rapidapi.com/get/basic?country=ar&tmdb_id=${title.tmdb_type}%2F${title.tmdb_id}&output_language=en`,
-            options
-          )
-            .then((response) => {
-              response.json();
-              if (
-                response.status === 429 &&
-                resultsContainer.children.length < 1
-              ) {
+    if (movieCheckbox.checked) {
+      fetch(url, { method: 'Get' })
+        .then((res) => res.json())
+        .then((json) => {
+          results = json.title_results;
+          results.forEach((title) => {
+            // Get where to watch
+            fetch(
+              `https://api.themoviedb.org/3/movie/${title.imdb_id}/watch/providers?api_key=110bb98a209c2c9716c472819fe63e72`,
+              options
+            )
+              .then((response) => response.json())
+              .then((response) => {
+                // Append results
                 const result = document.createElement('div');
                 result.classList.add('result');
-                result.textContent = `There was an error while searching: too many requests.`;
+                result.innerHTML = `<span class='search'>${title.name}</span> is on ${response.results.AR.flatrate[0].provider_name}.`;
                 resultsContainer.append(result);
-                return;
-              }
-            })
-            .then((response) => {
-              // console.log(response);
-
-              // Append results
-              if (response.streamingInfo.length > 0) {
-                const result = document.createElement('div');
-                result.classList.add('result');
-                result.textContent = `${response.title} is on ${response.streamingInfo[0]}.`;
-                resultsContainer.append(result);
-              }
-            })
-            .catch((err) => console.error(err));
+              })
+              .catch((err) => console.error(err));
+          });
         });
-      });
+    }
+
+    if (tvCheckbox.checked) {
+      fetch(url, { method: 'Get' })
+        .then((res) => res.json())
+        .then((json) => {
+          results = json.title_results;
+          results.forEach((title) => {
+            // Get where to watch
+            fetch(
+              `https://api.themoviedb.org/3/tv/${title.tmdb_id}/watch/providers?api_key=110bb98a209c2c9716c472819fe63e72`,
+              options
+            )
+              .then((response) => response.json())
+              .then((response) => {
+                // Append results
+                const result = document.createElement('div');
+                result.classList.add('result');
+                result.innerHTML = `<span class='search'>${title.name}</span> is on ${response.results.AR.flatrate[0].provider_name}.`;
+                resultsContainer.append(result);
+              })
+              .catch((err) => console.error(err));
+          });
+        });
+    }
   } else {
     // Error on input
     const result = document.createElement('div');
     result.textContent = "There's an error on your input.";
+    result.classList.add('result');
     resultsContainer.append(result);
+  }
+});
+
+clearBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  let resultsDivs = resultsContainer.children;
+  for (let item of resultsDivs) {
+    clearBtn.click();
+    item.remove();
   }
 });
